b95362282ffb6fbc85f78c4801407b278f5dc648
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-12-23 13:54:27 -0600

    Add default styles to app component

---
+++ src/challenges/001/index.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+
+const SignUp: React.FC = (): ReactElement => {
+  return(
+    <h1>hey</h1>
+  )
+}
+
+export default SignUp;
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css';
+import SignUp from '../../challenges/001'
 
 const App:React.FC = () => {
   return (
-    <div>
+    <div className='app' >
       I'm the app
     </div>
   );
