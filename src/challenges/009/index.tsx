0a00fa6b0ce8fd20590de7b64e46f1020ef32e17
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-04 13:56:12 -0600

    [update] 009 challenge

--- src/challenges/009/index.tsx
+++ src/challenges/009/index.tsx
@@ -1,12 +1,23 @@
 import React, { ReactElement } from 'react';
+import './009.css'
 
 const MusicPlayer: React.FC = (): ReactElement=> {
 
   return(
     <div className="container">
-      <div className="header"></div>
-      <div className="player"></div>
-      <div className="music-info"></div>
+      <div className="header">
+        <i> - </i>
+        <p>now playing</p>
+      </div>
+      <div className="player">
+        <small>3:48</small>
+        <div className="image"></div>
+        <input className='input' type="range"/>
+      </div>
+      <div className="music-info">
+        <h2>whatever it takes</h2>
+        <small>imagine dragons</small>
+      </div>
       <div className="player-options"></div>
     </div>
   )
