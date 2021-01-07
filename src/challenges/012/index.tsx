ee4679fd7415f016661b47cef7902e6a7dddb09d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-07 10:20:23 -0600

    [update] finish header

--- src/challenges/012/index.tsx
+++ src/challenges/012/index.tsx
@@ -1,14 +1,50 @@
-import React , { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 
-import './012.css'
+import './012.css';
 
-export const Shop:React.FC = (): ReactElement => {
+export const Header: React.FC = (): ReactElement => {
+	return (
+		<div className='header-container'>
+			<div className='left-side'>
+				<i className='fas fa-bars menu'></i>
+				<a href='/' className='link'>
+					men
+				</a>
+				<a href='/' className='link'>
+					woman
+				</a>
+				<a href='/' className='link'>
+					kids
+				</a>
+			</div>
+			<div className='logo-container'>
+      <i className="fab fa-drupal logo"></i>
+			</div>
+			<div className='right-side'>
+				<div className='search'>
+          <i className="fas fa-search"></i>
+					<p>search</p>
+				</div>
+				<i className="fas fa-shopping-bag shop"></i>
+			</div>
+		</div>
+	);
+};
 
-  return(
-    <h1>
-      I'm the shop
-    </h1>
-  )
-}
+// export const Content:React.FC = (): ReactElement => {
 
-export default Shop
+// }
+
+// export const Footer:React.FC = (): ReactElement => {
+
+// }
+
+export const Shop: React.FC = (): ReactElement => {
+	return (
+		<div className='container'>
+			<Header />
+		</div>
+	);
+};
+
+export default Shop;
