708a4a8f4307ec786fd03a81570e22c63134dda5
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-07 10:23:18 -0600

    [update]

--- src/challenges/012/index.tsx
+++ src/challenges/012/index.tsx
@@ -31,9 +31,14 @@ export const Header: React.FC = (): ReactElement => {
 	);
 };
 
-// export const Content:React.FC = (): ReactElement => {
+export const Content:React.FC = (): ReactElement => {
 
-// }
+  return(
+    <div className='content-container'>
+      <img src='https://images.unsplash.com/photo-1554735490-5974588cbc4f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' alt=''/>
+    </div>
+  )
+}
 
 // export const Footer:React.FC = (): ReactElement => {
 
@@ -43,6 +48,7 @@ export const Shop: React.FC = (): ReactElement => {
 	return (
 		<div className='container'>
 			<Header />
+      <Content />
 		</div>
 	);
 };
