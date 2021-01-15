9036ac1a6bf51ce30e365bb67de0c739ec33cbac
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-15 11:29:51 -0600

    [finish] finsih 18 model

--- src/challenges/018/index.tsx
+++ src/challenges/018/index.tsx
@@ -2,10 +2,37 @@ import React, { ReactElement } from 'react';
 
 import './018.css';
 
+const Content: React.FC = (): ReactElement => {
+	return (
+		<div className='content-container'>
+			<div className='chart'>
+				<p className='chart-content'>4,4</p>
+			</div>
+			<div className='content-info'>Radial chart (dark)</div>
+		</div>
+	);
+};
+
+const Data: React.FC = (): ReactElement => {
+	return (
+		<div className='data-container'>
+			<div className='side'>
+				<div className='up'>78,2%</div>
+				<div className='down'>item</div>
+			</div>
+			<div className='side'>
+				<div className='up'>21,8%</div>
+				<div className='down'>item</div>
+			</div>
+		</div>
+	);
+};
+
 export const Chart: React.FC = (): ReactElement => {
 	return (
 		<div className='container'>
-			<h1>Hey</h1>
+			<Content />
+			<Data />
 		</div>
 	);
 };
